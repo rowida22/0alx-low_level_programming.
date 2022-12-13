e34c91683b63631b2c97d8002bdbc9ff6be302f8
rowida22 <rowida91200@gmail.com>
2022-12-13 13:48:57 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -print alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+
+	_putchar('\n');
+}
