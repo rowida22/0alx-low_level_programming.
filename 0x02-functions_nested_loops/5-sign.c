d8c2244a623b8599f570edad36f22e8335c4b5a2
rowida22 <rowida91200@gmail.com>
2022-12-13 14:44:48 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - return 0 if letter not lowercase, 1 if letter lowercase
+ * @n : number to check
+ * return : 0 or 1
+ */
+int print_sign(int n)
+{
+	int check;
+
+	if (n > 0)
+	{
+		check = 1;
+	        _putcahr('+');
+	}
+	else if (n == 0)
+	{
+		check = 0;
+		_putchar('0');
+	}
+	else (n < 0)
+	{
+		check = -1;
+		_putchar('-');
+	}
+}
+
