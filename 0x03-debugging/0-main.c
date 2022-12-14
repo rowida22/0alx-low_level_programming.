c56f6b3c793cb5c7a9946fc012c8d4420bc492d1
rowida22 <rowida91200@gmail.com>
2022-12-14 10:19:47 -0800

    Call funn

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * main - test function if int is positive or negative
+ * main - function check if int is positive or negative
  *
  * Return: 0
  */
-
 int main(void)
 {
 	int i = 0;
-
 	positive_or_negative(i);
+
 	return (0);
 }
