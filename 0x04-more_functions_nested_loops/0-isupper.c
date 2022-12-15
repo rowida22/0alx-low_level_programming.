01bb143d6ae83f93bafa08101a670647886df044
rowida22 <rowida91200@gmail.com>
2022-12-15 06:32:23 -0800

    Check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,19 @@
 
 /**
  * main - check if char is uppercase or lowercase
+ * @c: parameter
  * return : 0 Done
+ *
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 65 && c < 91)
 	{
-		return (1):
+		return (1);
 	}
 	else
 	{
-		return (0):
+		return (0);
 	}
 }
