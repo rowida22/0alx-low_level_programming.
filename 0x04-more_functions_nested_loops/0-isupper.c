4090fd0350f02f5ed74122d784ad62d6d0f7afa1
rowida22 <rowida91200@gmail.com>
2022-12-15 06:28:14 -0800

    check if uppercase or lowercse

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check if char is uppercase or lowercase
+ * return : 0 Done
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1):
+	}
+	else
+	{
+		return (0):
+	}
+}
