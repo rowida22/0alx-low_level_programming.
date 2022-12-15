874319dfa8de33620187df2330c0bd5be9e0715f
rowida22 <rowida91200@gmail.com>
2022-12-15 09:28:49 -0800

    Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  draws a diagonal line
+ * Return: \n
+ */
+
+void print_diagonal(int n)
+{
+	int i = 1;
+
+	if (i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
