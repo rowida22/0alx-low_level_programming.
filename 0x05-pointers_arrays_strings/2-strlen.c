c8a061e2a83be5a5ec221fefe1a0bd391e96aece
rowida22 <rowida91200@gmail.com>
2022-12-19 12:25:58 -0800

    length of string using i to loop in str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: parameter
+ * return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if(s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
