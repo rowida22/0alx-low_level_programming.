8a93175a76d0181a70b409428492ba28480141e5
rowida22 <rowida91200@gmail.com>
2022-12-19 15:27:23 -0800

    Done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _puts : function that prints a string, followed by a new line,
+ * _puts - function that prints a string, followed by a new line, to stdout
  * @str: pointer val
  * Return: 0
  */
