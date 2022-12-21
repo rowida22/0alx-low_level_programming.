a033d4ea1751cf357edb6f8b57ea8a8e1f894d6f
rowida22 <rowida91200@gmail.com>
2022-12-21 15:22:52 -0800

    Concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int length, j;
-	
+
 	length = 0;
 
 	while (dest[length] != '\0')
@@ -22,6 +22,7 @@ char *_strcat(char *dest, char *src)
 	{
 		dest[length] = src[j];
 	}
+
 	dest[length] = '\0';
 	return (dest);
 }
